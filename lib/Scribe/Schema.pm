072173bf1d7691d12a466d159bba92365d1b6305
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-05 01:24:42 +0000

    DBIx::Class::Schema based module for managing database objects

---
+++ lib/Scribe/Schema.pm
@@ -0,0 +1,22 @@
+package Scribe::Schema;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Schema/;
+
+__PACKAGE__->load_namespaces();
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe::Schema
+
+=head1 SYNOPSIS
+
+This module inherits from DBIx::Class:Schema
+
+=cut
