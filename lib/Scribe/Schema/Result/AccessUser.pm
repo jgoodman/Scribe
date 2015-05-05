76d9b85a44020a3529a5ea65a5d43e6bcefa049b
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-05 01:26:14 +0000

    DBIx::Class::Schema::Result based module to manage access_user database objects

---
+++ lib/Scribe/Schema/Result/AccessUser.pm
@@ -0,0 +1,33 @@
+package Scribe::Schema::Result::AccessUser;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('access_user');
+__PACKAGE__->add_columns(
+    access_user_id => { data_type => 'serial', is_nullable => 0, },
+    name           => { data_type => 'text',   is_nullable => 0, },
+    password       => { data_type => 'text',   is_nullable => 0, },
+    email          => { data_type => 'text',   is_nullable => 1, },
+);
+
+__PACKAGE__->set_primary_key('access_user_id');
+__PACKAGE__->add_unique_constraint(access_user_unique_key_name => [ qw/name/ ]);
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe::Schema::Result::AccessUser
+
+=head1 SYNOPSIS
+
+This module abstracts a user who can log into the application
+
+=head1 METHODS
+
+=cut
