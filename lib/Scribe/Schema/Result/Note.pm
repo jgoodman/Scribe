d4961d51c283ed19713e6a9e205b3acbd1d06771
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-05 01:28:45 +0000

    DBIx::Class::Schema::Result based module to manage note database objects

---
+++ lib/Scribe/Schema/Result/Note.pm
@@ -0,0 +1,34 @@
+package Scribe::Schema::Result::Note;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('note');
+__PACKAGE__->add_columns(
+    note_id  => { data_type => 'serial',  is_nullable => 0, },
+    name     => { data_type => 'text',    is_nullable => 1, },
+    text     => { data_type => 'text',    is_nullable => 1, },
+    label_id => { data_type => 'integer', is_nullable => 1, },
+    weight   => { data_type => 'integer', is_nullable => 1, },
+);
+
+__PACKAGE__->set_primary_key('note_id');
+__PACKAGE__->belongs_to(label => 'Scribe::Schema::Result::Label', 'label_id', { join_type => 'left' });
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe::Schema::Result::Note
+
+=head1 SYNOPSIS
+
+This module abstracts "note"
+
+=head1 METHODS
+
+=cut
