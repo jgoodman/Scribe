2bad8570a3794dd4cc4f1e94ccd5261d24a1ffd4
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-05 01:27:07 +0000

    DBIx::Class::Schema::Result based module to manage character_goal database objects

---
+++ lib/Scribe/Schema/Result/CharacterGoal.pm
@@ -0,0 +1,32 @@
+package Scribe::Schema::Result::CharacterGoal;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('character_goal');
+__PACKAGE__->add_columns(
+    character_id => { data_type => 'integer', is_nullable => 0, },
+    goal_id      => { data_type => 'integer', is_nullable => 0, },
+);
+
+__PACKAGE__->add_unique_constraint([ qw/character_id goal_id/ ]);
+__PACKAGE__->belongs_to(character => 'Scribe::Schema::Result::Character', 'character_id', { join_type => 'left' });
+__PACKAGE__->belongs_to(goal      => 'Scribe::Schema::Result::Goal',      'goal_id',      { join_type => 'left' });
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe::Schema::Result::CharacterGoal
+
+=head1 SYNOPSIS
+
+This module abstracts "character_goal"
+
+=head1 METHODS
+
+=cut
