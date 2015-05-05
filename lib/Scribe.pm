cd228bd8eeec90d9e3f5609d8ddc381432ec0114
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-05 01:09:03 +0000

    Scribe module used for namespacing

---
+++ lib/Scribe.pm
@@ -0,0 +1,18 @@
+package Scribe;
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe
+
+=head1 SYNOPSIS
+
+A collection of modules intended to provide tools for creative writing.
+
+=cut
