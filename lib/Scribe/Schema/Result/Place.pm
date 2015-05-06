b23cde26634864360d3cac0616f8d61428116a6f
Joshua Goodman <jgoodman1990@gmail.com>
2015-05-06 03:47:39 +0000

    Database object for managing records in "place" table

---
+++ lib/Scribe/Schema/Result/Place.pm
@@ -0,0 +1,30 @@
+package Scribe::Schema::Result::Place;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class::Core/;
+
+__PACKAGE__->table('place');
+__PACKAGE__->add_columns(
+    place_id => { data_type => 'serial', is_nullable => 0, },
+    name     => { data_type => 'text',   is_nullable => 1, },
+);
+
+__PACKAGE__->set_primary_key('place_id');
+
+1;
+
+__END__
+
+=head1 NAME
+
+Scribe::Schema::Result::Place
+
+=head1 SYNOPSIS
+
+This module abstracts "place"
+
+=head1 METHODS
+
+=cut
